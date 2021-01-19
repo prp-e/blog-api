375cd7beb46007a2ec6d193457ba8260fc538711
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-01-19 09:47:07 +0330

    Designed the table responsible for the posts

--- database/migrations/2021_01_19_061421_create_posts_table.php
+++ database/migrations/2021_01_19_061421_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug'); 
+            $table->text('body');
             $table->timestamps();
         });
     }
