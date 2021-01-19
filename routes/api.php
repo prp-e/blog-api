eb70510b4375225eb3ec15cc9ba4ac5a0792fbdd
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-01-19 10:09:02 +0330

    Added routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
