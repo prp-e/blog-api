328da142a4b2a2261fb9d683d27354fdaf83df8e
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-01-19 10:15:37 +0330

    Got it right

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('posts', 'PostController');
+Route::resource('posts', PostController::class);
