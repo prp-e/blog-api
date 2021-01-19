888ca96b0f17b08895c165741c81feba8be69d2f
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-01-19 14:02:21 +0330

    feat: added user id to posts

--- database/migrations/2021_01_19_061421_create_posts_table.php
+++ database/migrations/2021_01_19_061421_create_posts_table.php
@@ -15,6 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
             $table->string('title');
             $table->string('slug'); 
             $table->text('body');
